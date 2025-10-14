d656e1adcb173c1d471cc1e7beb646e7dc14d113
Erez Freiberger <enoodle@gmail.com>
2025-10-14 21:25:17 +0200

    feat: configuration of runtime class for reservation pods

--- pkg/binder/binding/resourcereservation/resource_reservation.go
+++ pkg/binder/binding/resourcereservation/resource_reservation.go
@@ -42,8 +42,6 @@ const (
 	unknownGpuIndicator            = "-1"
 )
 
-var runtimeClassName = "nvidia"
-
 type service struct {
 	fakeGPuNodes        bool
 	kubeClient          client.WithWatch
@@ -54,6 +52,7 @@ type service struct {
 	serviceAccountName  string
 	appLabelValue       string
 	scalingPodNamespace string
+	runtimeClassName    string
 }
 
 func NewService(
@@ -65,6 +64,7 @@ func NewService(
 	serviceAccountName string,
 	appLabelValue string,
 	scalingPodNamespace string,
+	runtimeClassName string,
 ) *service {
 	return &service{
 		fakeGPuNodes:        fakeGPuNodes,
@@ -451,7 +451,7 @@ func (rsc *service) createResourceReservationPod(
 		},
 		Spec: v1.PodSpec{
 			NodeName:           nodeName,
-			RuntimeClassName:   &runtimeClassName,
+			RuntimeClassName:   &rsc.runtimeClassName,
 			ServiceAccountName: rsc.serviceAccountName,
 			Containers: []v1.Container{
 				{
